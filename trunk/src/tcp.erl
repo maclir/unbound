1b0b08a47c9504b58a3551b279c34de1a925e755
evelina <evelina@18a80963-79fc-5ace-71d3-e8cd1e56baa2>
2011-11-14 19:21:26 +0000

    some changes for the file_writer

--- trunk/src/tcp.erl
+++ trunk/src/tcp.erl
@@ -1,6 +1,6 @@
 -module(tcp).
 -import(bencode, [decode/1, encode/1]).
--export([server/0, wait_connect/2, get_request/4, handle/1, client/1, send/2, connect_to_server/3, open_a_socket/4, connect_to_client/4]).
+-export([server/0, wait_connect/2, get_request/4, handle/1, client/1, send/2, connect_to_server/3, open_a_socket/7, connect_to_client/7]).
 
 %%
 %% Tracker communiacation
@@ -37,7 +37,7 @@ separate(<<Ip1:8, Ip2:8, Ip3:8, Ip4:8,Port:16,Rest/binary>>)->
 %% Peer Communication
 %%
 
-open_a_socket(DestinationIp, DestinationPort,InfoHash,ClientId)->
+open_a_socket(DestinationIp, DestinationPort,InfoHash,ClientId, Name, Shas, Piece_length)->
     {ok,Socket}=gen_tcp:connect(DestinationIp, DestinationPort, [binary, {packet,0}]), %% pay attention to {packet,0}.
     
     %% when this parameter is set to 0, no packaging is done.
@@ -46,11 +46,11 @@ open_a_socket(DestinationIp, DestinationPort,InfoHash,ClientId)->
     
     case whereis(slave) of %% we make a separate process for communiacation with a peer
 	undefined ->
-	    spawn(?MODULE, connect_to_client,[self(), Socket,InfoHash,ClientId])
+	    spawn(?MODULE, connect_to_client,[self(), Socket,InfoHash,ClientId, Name, Shas, Piece_length])
     end,
     Socket.
 
-connect_to_client(MasterPid, Socket,InfoHash,ClientId)-> 
+connect_to_client(MasterPid, Socket,InfoHash,ClientId, Name, Shas, Piece_length)-> 
     erlang:port_connect(Socket, self()), %% since the port was opened it another process, we have to reconnect it to the current process.
     gen_tcp:send(Socket,[  %% sending a handshake
 			   19,
@@ -61,7 +61,7 @@ connect_to_client(MasterPid, Socket,InfoHash,ClientId)->
 						]),
 	handshake_loop(Socket, MasterPid), %% starting a loop for handling handshaking
 	inet:setopts(Socket, [{packet, 4}]), %% if you visited the link above, this should be more or less clear
-	main_loop(Socket, MasterPid). %% starting the main loop for further communiation
+	main_loop(Socket, MasterPid, Name, Shas, Piece_length). %% starting the main loop for further communiation
 	
 handshake_loop(Socket, MasterPid)->
 	receive
@@ -80,37 +80,37 @@ handshake_loop(Socket, MasterPid)->
 
 %% this loop processes ALL messages. The ones it gets from the peer AND the ones we send to it, from the parent process
 %% to send a message from the parent process, as a common structure: <process name> ! <message body>, for example: slave ! keep_alive.	
-main_loop(Socket, MasterPid)->
+main_loop(Socket, MasterPid, Name, Shas, Piece_length)->
 	receive
 		keep_alive ->	%% when we send keep_alive message to the slave
 			gen_tcp:send(Socket,<<>>), %% it sends the appropriate message to the peer
-			main_loop(Socket,MasterPid); %% starts the loop again
+			main_loop(Socket,MasterPid, Name, Shas, Piece_length); %% starts the loop again
 		interested ->
 			gen_tcp:send(Socket,<<2>>), %% send a message, to say you are interested. for more info see bittorrent specification
-			main_loop(Socket,MasterPid);
+			main_loop(Socket,MasterPid, Name, Shas, Piece_length);
 		{piece, Index, Offset, Length} ->
 			gen_tcp:send(Socket, [<<6:8, Index:32, Offset:32, Length:32>>]), %% this is the request syntax. this is how we request a piece
-			main_loop(Socket,MasterPid);
+			main_loop(Socket,MasterPid, Name, Shas, Piece_length);
 		{tcp,_,<<>>}-> 
 			MasterPid ! got_keep_alive, %% messages, having a structre like this {tcp,_,_} show that they were recieved from the peer.
-			main_loop(Socket, MasterPid); %% in this case <<>> actually means keep_alive message
+			main_loop(Socket, MasterPid, Name, Shas, Piece_length); %% in this case <<>> actually means keep_alive message
 		{tcp,_,<<1>>}-> 
 			MasterPid ! got_unchoked, %% process an unchoked message
-			main_loop(Socket, MasterPid);
+			main_loop(Socket, MasterPid, Name, Shas, Piece_length);
 		{tcp,_,<<_LengthPrefix:7/binary, %% pattern matching the response, when we requested a piece
 				_PieceIndex:1/binary,	%% variables names speak for themselves
 				_Offset:1/binary,		%% for more info see the specification
 				Block/binary>>}->
 			MasterPid ! {"got the block: ", Block}, 
-			main_loop(Socket, MasterPid);
+			main_loop(Socket, MasterPid, Name, Shas, Piece_length);
 		{tcp,_,M}->   
 			MasterPid ! {"got this during main loop:", M}, %% this is for the case when we got some other unrecognized message from a peer
-			main_loop(Socket, MasterPid);
+			main_loop(Socket, MasterPid, Name, Shas, Piece_length);
 		stop ->
 			MasterPid ! stopping; %% this is made to stop the slave process. The loop is not being called again here.
 		Smth ->
 			MasterPid ! {"got unknown message:",Smth}, %% in case we got something really weird
-			main_loop(Socket, MasterPid)
+			main_loop(Socket, MasterPid, Name, Shas, Piece_length)
 	end. %% there is no recieving loop for the parent process, so, to actually see, what the slave sends to you, use the flush() function.
 	
 %%
