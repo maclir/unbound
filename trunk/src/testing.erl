97cef655f7ceebb88c7b5e7e3d211da110a0dc58
alireza <alireza@18a80963-79fc-5ace-71d3-e8cd1e56baa2>
2011-11-23 19:05:37 +0000

    testing module

--- trunk/src/testing.erl
+++ trunk/src/testing.erl
@@ -3,6 +3,6 @@
 -export([start/0]).
 
 start() ->
-	{ok, File} = file:read_file("M3_test.torrent"),
+	{ok, File} = file:read_file("M3_TestCar.torrent"),
 	spawn(com_central,start_link,[]),
 	com_central:add_new_torrent_file(File).
